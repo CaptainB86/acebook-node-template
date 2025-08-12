b261e00bb9e92be5d9292d6e086270fac15d0272
eanna durham <eannadurham@gmail.com>
2025-08-12 15:49:50 +0100

    updated start.sh and www.js

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,17 +1,21 @@
 #!/usr/bin/env bash
 set -euxo pipefail
+
 cat >/etc/systemd/system/acebook.service <<'UNIT'
 [Unit]
 Description=Acebook App
 After=network.target
+
 [Service]
 WorkingDirectory=/opt/acebook
-ExecStart=/usr/bin/node app.js
+ExecStart=/usr/bin/node bin/www
 Restart=always
 Environment=PORT=80 NODE_ENV=production
 [Install]
 WantedBy=multi-user.target
 UNIT
+
 systemctl daemon-reload
 systemctl enable acebook
 systemctl restart acebook
+
